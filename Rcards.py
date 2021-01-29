a3be956c31c5a9d838d0ec076b8fd65bf880411a
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-01-29 16:54:10 -0500

    Update Rcards.py

--- Rcards.py
+++ Rcards.py
@@ -2,6 +2,9 @@
 #Fri Jan 29 03:29:31 PM EST 2021
 from random import randrange
 from random import shuffle
+import namegenerator
+
+print ()
 
 #variables
 handOneSize = 0;
@@ -10,25 +13,28 @@ deckSize = 15;
 deck=[]
 deckOne = []
 handOne = []
-healthOne = []
+healthOne = 100;
+healthTwo = 100;
 handOneView = ""
 handTwoView = ""
 xx=0;
 emoji = ["🀄🃏🎴💾💽💿📀⛔"];
-element = ["♈","♉","♊","♋","♌","♍","♎","♏","♐","♑"];
-selectionElement = ""
+signs = ["[Aries ♈]","[Taurus ♉]","[Gemini ♊]","[Cancer ♋]","[Leo ♌]","[Virgo ♍]","[Libra ♎]","[Scorpius ♏]","[Sagittarius ♐]","[Capricornus ♑]"];
+selectionsign = ""
+
 
 class cardMonster:
-    def __init__(self, id, attack, deffence, element, strAffectedBy, defAffectedBy, infAttack, infDeffence):
+    def __init__(self, id, attack, deffence, signs, strAffectedBy, defAffectedBy, infAttack, infDeffence):
         self.id = id;
         self.attack = attack;
         self.deffence = deffence;
-        self.element = element;
-        self.strAffectedBy = strAffectedBy;
-        self.defAffectedBy = defAffectedBy;
+        self.signs = signs;
+        self.strAffectedBy = strAffectedBy*2;
+        self.defAffectedBy = defAffectedBy*2;
         self.infAttack = infAttack;
         self.infDeffence = infDeffence;
         self.type="monster"
+        self.name = namegenerator.gen();
 
 class cardTerrain:
     def __init__(self, id, weakerAgainst, strongerAgainst, infAttack, infDeffence):
@@ -38,6 +44,7 @@ class cardTerrain:
         self.infAttack = infAttack;
         self.infDeffence = infDeffence;
         self.type="Terain"
+        self.name = namegenerator.gen();
 
 #generate deck
 for x in range(deckSize):
@@ -67,35 +74,56 @@ for x in range(handTwoSize):
 print(handTwoView);
 
 #field
-#field=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+field=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+def display():
+    linetxt=""
+    for y in range(4):
+        print(linetxt);
+        linetxt="";
+        for x in range(3):
+            if x==xx and y ==yy:
+				linetxt+=colored("#",'red');
+            elif field[x][y]==0:
+                linetxt+=colored(".",'white');
+            else:
+                linetxt+=colored("🎴",'white');
 
-#player one hand
-handOneSize=len(handOne);
-for x in range(handOneSize):
-    if x == xx:
-        handOneView += "🃏";
+
+def displayHandOne():
+    global handOneView
+    global xx
+    global handOne
+    #player one hand
+    handOneSize=len(handOne);
+    for x in range(handOneSize):
+        if x == xx:
+            handOneView += "🃏";
+        else:
+            handOneView += "🎴";
+    print(handOneView);
+
+    if(handOne[xx].type=="monster"):
+        selectionsign = str(signs[handOne[xx].signs]);
+        name = str(handOne[xx].name);
+        print("Name: "+name);
+        print("Type: Monster");
+        print("Sign: "+selectionsign);
+        print("Attack="+str(handOne[xx].attack)+" Deffence="+str(handOne[xx].deffence));
+
+        signsAttack = str(signs[handOne[xx].strAffectedBy]);
+        if(handOne[xx].infAttack>0):
+            print("Attack +"+str(handOne[xx].infAttack)+" against "+signsAttack);
+        elif(handOne[xx].infAttack<0):
+            print("Attack "+str(handOne[xx].infAttack)+" against "+signsAttack);
+
+
+        signsDeffence = str(signs[handOne[xx].defAffectedBy]);
+        if(handOne[xx].infDeffence>0):
+            print("Deffence +"+str(handOne[xx].infDeffence)+" against "+signsDeffence);
+        elif(handOne[xx].infDeffence<0):
+            print("Deffence "+str(handOne[xx].infDeffence)+" against "+signsDeffence);
     else:
-        handOneView += "🎴";
-print(handOneView);
+        print("Card type: Terrain");
+        print("id="+str(handOne[xx].id)+" attack="+str(handOne[xx].weakerAgainst)+" deffence="+str(handOne[xx].strongerAgainst))
 
-if(handOne[xx].type=="monster"):
-    selectionElement = str(element[handOne[xx].element]);
-    print("Card type: Monster");
-    print("Card element: "+selectionElement);
-    print("Attack="+str(handOne[xx].attack)+" Deffence="+str(handOne[xx].deffence));
-
-    elementAttack = str(element[handOne[xx].strAffectedBy]);
-    if(handOne[xx].infAttack>0):
-        print("Attack +"+str(handOne[xx].infAttack)+" against "+elementAttack);
-    elif(handOne[xx].infAttack<0):
-        print("Attack "+str(handOne[xx].infAttack)+" against "+elementAttack);
-
-
-    elementDeffence = str(element[handOne[xx].defAffectedBy]);
-    if(handOne[xx].infDeffence>0):
-        print("Deffence +"+str(handOne[xx].infDeffence)+" against "+elementDeffence);
-    elif(handOne[xx].infDeffence<0):
-        print("Deffence "+str(handOne[xx].infDeffence)+" against "+elementDeffence);
-else:
-    print("Card type: Terrain");
-    print("id="+str(handOne[xx].id)+" attack="+str(handOne[xx].weakerAgainst)+" deffence="+str(handOne[xx].strongerAgainst))
+displayHandOne();

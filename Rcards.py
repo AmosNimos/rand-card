824c4b2855894a8d62b6fa2bc5b1bf49504eb7e0
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-01-29 13:05:35 -0500

    Add files via upload

---
+++ Rcards.py
@@ -0,0 +1,44 @@
+from random import randrange
+from random import shuffle
+
+#variables
+handSize = 5;
+deckSize = 15;
+deck=[]
+class cardMonster:
+    def __init__(self, id, attack, deffence, element, weakAgainst, strongAgainst, Influence):
+    self.id = id;
+    self.attack = attack;
+    self.deffence = deffence;
+    self.element = element;
+    self.weakAgainst = weakAgainst;
+    self.strongAgainst = strongAgainst;
+    self.Influence = Influence;
+    self.type="monster"
+
+class cardTerain:
+    def __init__(self, id, weakerAgainst, strongerAgainst, Influence):
+    self.id = id;
+    self.weakerAgainst = weakerAgainst;
+    self.strongerAgainst = strongerAgainst;
+    self.Influence = Influence;
+    self.type="Terain"
+
+for x in range(deckSize):
+    if(random)
+    deck.append(card(x,randrange(0,9),randrange(0,9),randrange(0,9),randrange(0,9),randrange(0,9),randrange(0,9)));
+
+deckOne=shuffle(deck);
+handOne = deckOne[-3:];
+deckOne = deckOne[:-3];
+
+field[[0,0,0,0,0].[0,0,0,0,0].[0,0,0,0,0].[0,0,0,0,0]]
+
+#player two hand
+print("#####")
+
+#field
+
+#player one hand
+print("#####")
+#card info
